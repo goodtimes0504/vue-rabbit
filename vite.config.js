e5fc6dd76a2579d2b7a29e2d97e56a808c58116a
goodtimes0504 <revolution_lc@outlook.com>
2024-11-18 17:43:35 +0800

    element-plus的自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+//引入element-plus 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // ...下面这俩是element-plus按需导入的
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     //实际的路径转换 @ =》src
