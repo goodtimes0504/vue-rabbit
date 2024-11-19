c36e63f685947cea451f3d8df0edfece48e009a6
goodtimes0504 <revolution_lc@outlook.com>
2024-11-19 12:04:02 +0800

    吸顶导航交互实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,8 +3,11 @@
 import LayoutNav from "../Layout/components/LayoutNav.vue"
 import LayoutHeader from "../Layout/components/LayoutHeader.vue"
 import LayoutFooter from "../Layout/components/LayoutFooter.vue"
+//引入吸顶导航功能
+import LayoutFixed from "../Layout/components/LayoutFixed.vue"
 </script>
 <template>
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <!-- 二级路由出口 -->
