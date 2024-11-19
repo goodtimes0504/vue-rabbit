3a4930ae8b0921ae2c0b6d876e08cb5942b701b8
goodtimes0504 <revolution_lc@outlook.com>
2024-11-19 14:49:19 +0800

    goods-item组件封装

--- src/directives/index.js
+++ src/directives/index.js
@@ -9,7 +9,7 @@ export const lazyPlugin = {
       mounted(el, binding) {
         //el:指令绑定的那个元素
         //binding:binding.value 指令等于号后面绑定的表达式的值 图片url
-        console.log(el, binding)
+        // console.log(el, binding)
         const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
           //   console.log(isIntersecting)
           if (isIntersecting) {
