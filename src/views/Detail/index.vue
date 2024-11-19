ea8004b5107b61bade5cc0849c56852aa8c13fe2
goodtimes0504 <revolution_lc@outlook.com>
2024-11-19 19:48:39 +0800

    详情页基础渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,15 +1,46 @@
 <script setup>
+//导入获取商品详情的接口
+import { getDetail } from "@/apis/detail"
+//导入ref和onMounted
+import { ref, onMounted } from "vue"
+//导入路由 获取路由参数
+import { useRoute } from "vue-router"
+//获取路由参数
+const route = useRoute()
+
+//定义响应式数据保存商品详情
+const goods = ref({})
+
+//定义一个函数用来获取商品详情
+const getGoods = async () => {
+  const res = await getDetail(route.params.id)
+  goods.value = res.result
+}
+
+//在组件挂载时调用函数获取商品详情
+onMounted(() => {
+  return getGoods()
+})
 </script>
 
 <template>
   <div class="xtx-goods-page">
-    <div class="container">
+    <div class="container" v-if="goods.details">
       <div class="bread-container">
         <!-- 面包屑导航 -->
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">母婴 </el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">跑步鞋 </el-breadcrumb-item>
+          <!-- 这里报错原始时 goods一开始时空对象 空对象里的属性也是空的 自然没有1的索引 undefined[1] 报错
+           解决办法：1 可选链 ？ 2 v-if判断
+          -->
+          <el-breadcrumb-item
+            :to="{ path: `/category/${goods.categories[1].id}` }"
+            >{{ goods.categories[1].name }}
+          </el-breadcrumb-item>
+          <el-breadcrumb-item
+            :to="{ path: `/category/sub/${goods.categories[0].id}` }"
+            >{{ goods.categories[0].name }}
+          </el-breadcrumb-item>
           <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
@@ -24,33 +55,33 @@
               <ul class="goods-sales">
                 <li>
                   <p>销量人气</p>
-                  <p>100+</p>
+                  <p>{{ goods.salesCount }}+</p>
                   <p><i class="iconfont icon-task-filling"></i>销量人气</p>
                 </li>
                 <li>
                   <p>商品评价</p>
-                  <p>200+</p>
+                  <p>{{ goods.commentCount }}+</p>
                   <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
                 </li>
                 <li>
                   <p>收藏人气</p>
-                  <p>300+</p>
+                  <p>{{ goods.collectCount }}+</p>
                   <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <p>400+</p>
+                  <p>{{ goods.brand.name }}</p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
             </div>
             <div class="spec">
               <!-- 商品信息区 -->
-              <p class="g-name">抓绒保暖，毛毛虫儿童鞋</p>
-              <p class="g-desc">好穿</p>
+              <p class="g-name">{{ goods.name }}</p>
+              <p class="g-desc">{{ goods.desc }}</p>
               <p class="g-price">
-                <span>200</span>
-                <span> 100</span>
+                <span>{{ goods.oldPrice }}</span>
+                <span> {{ goods.price }}</span>
               </p>
               <div class="g-service">
                 <dl>
@@ -87,12 +118,21 @@
                 <div class="goods-detail">
                   <!-- 属性 -->
                   <ul class="attrs">
-                    <li v-for="item in 3" :key="item.value">
-                      <span class="dt">白色</span>
-                      <span class="dd">纯棉</span>
+                    <li
+                      v-for="item in goods.details.properties"
+                      :key="item.value"
+                    >
+                      <span class="dt">{{ item.name }}</span>
+                      <span class="dd">{{ item.value }}</span>
                     </li>
                   </ul>
                   <!-- 图片 -->
+                  <img
+                    v-for="img in goods.details.pictures"
+                    :src="img"
+                    :key="img"
+                    alt=""
+                  />
                 </div>
               </div>
             </div>
