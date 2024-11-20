1caa1a8493f4110f75f3b635b5c054d570deca5c
goodtimes0504 <revolution_lc@outlook.com>
2024-11-20 14:12:53 +0800

    pinia插件使数据持久化

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,21 +2,27 @@
 import { defineStore } from "pinia"
 import { loginAPI } from "@/apis/user"
 import { ref, computed } from "vue"
-export const useUserStore = defineStore("user", () => {
-  //定义state
-  const userInfo = ref({})
-  //定义action
-  const getUserInfo = async (data) => {
-    const result = await loginAPI(data)
-    userInfo.value = result.result
-  }
-  //   //定义getters
-  //   const isLogin = computed(() => {
-  //     return !!user.value
-  //   })
-  return {
-    userInfo,
-    getUserInfo,
-    // isLogin,
-  }
-})
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    //定义state
+    const userInfo = ref({})
+    //定义action
+    const getUserInfo = async (data) => {
+      const result = await loginAPI(data)
+      userInfo.value = result.result
+    }
+    //   //定义getters
+    //   const isLogin = computed(() => {
+    //     return !!user.value
+    //   })
+    return {
+      userInfo,
+      getUserInfo,
+      // isLogin,
+    }
+  },
+  {
+    persist: true,
+  },
+)
